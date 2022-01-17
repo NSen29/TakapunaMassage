38dd946c10e4fce30266ede96135fbb8206f7ed1
nst2000 <sennoel2000@outlook.co.nz>
2022-01-17 18:00:46 +1300

    Cards with Buttons
    Simple card layout, animations not working, need to place them horizontally

--- takapuna-massage-frontend/src/pages/pricing.js
+++ takapuna-massage-frontend/src/pages/pricing.js
@@ -1,54 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './price.css'
 
 export class pricing extends Component {
     render() {
         return ( <
             div >
             <
-            h1 > pricing page < /h1>  <
-            PriceCard
+            PriceCard service = 'Upper and Lower Body'
+            image = 'https://lh5.googleusercontent.com/WczeYaBDBshhUWwYqDjC7AkAVO_vWhanxsNWF7S2xorWsT67jk3Y-yjxbyaW_Fq146xquS1BTv2bBbtPxZA3f8nYNMXcKTrwKiyJT2YMrmEkYyqTwsqYF_sDoN-MaXWDAA=w1280'
+            body = 'Neck, Shoulders and Back: Back and Leg 30 min sessions for $50 / 45 min sessions for $70 / 60 min sessions for $85 ' / >
 
-            img = "https://lh4.googleusercontent.com/nluDrcI4E_PuXScKKYAe4-O9A0b0ZJ_r4gEvB6WKwFvxwlxdQOwgyWUvqBYHq9yq_-5pP9TbukzR5hmVvTtpjhmHHSjXCNplNJGgyaKffGFAew2_cLxdn23t-CjDy0Bxgw=w1280"
-            name = "Head, Neck and Shoulders"
-            description = "10mins          $20  20mins           $30  30mins           $50" /
-            >
             <
-            PriceCard
-
-            img = "https://lh5.googleusercontent.com/WczeYaBDBshhUWwYqDjC7AkAVO_vWhanxsNWF7S2xorWsT67jk3Y-yjxbyaW_Fq146xquS1BTv2bBbtPxZA3f8nYNMXcKTrwKiyJT2YMrmEkYyqTwsqYF_sDoN-MaXWDAA=w1280"
-            name = "Upper/Lower Body"
-            description = "30mins          $50  45mins           $70  60mins           $85" /
+            PriceCard service = 'Whole Body'
+            image = 'https://lh4.googleusercontent.com/i6Z20_FPY1w3_JdXrrPJb692HeSh0b9ayN_Z4Kpag3iP_l_CPa6DIejwN_oisMGyeNNT5I7_L_uohzfaWyzCc9KFcFWwz0jI6bncDRTzqOF39TDtfyAa1dx2nD3-NpvwkA=w1280'
+            body = '45 min sessions  for  $70 / 60 min sessions for $85, 90 min sessions for $125 /120 min sessions for $160' /
             >
+
             <
-            PriceCard
-
-            img = "https://lh4.googleusercontent.com/i6Z20_FPY1w3_JdXrrPJb692HeSh0b9ayN_Z4Kpag3iP_l_CPa6DIejwN_oisMGyeNNT5I7_L_uohzfaWyzCc9KFcFWwz0jI6bncDRTzqOF39TDtfyAa1dx2nD3-NpvwkA=w1280"
-            name = "Head, Neck and Shoulders"
-            description = "10mins          $20  20mins           $30  30mins           $50" /
-            >
+            PriceCard service = 'Reflexology Foot Massage'
+            image = 'https://lh3.googleusercontent.com/KNubFiT0x1OnSjQovVrTXTXYV9HYacCd5fL1bJEtlJ3DLGJd4rJ3pc4KOwr45QAc3rhlPHmKyFwnCTjQTGKefKGG8geTuuL58QeiqmeYnCOZVR0biXzSNeYRT0wBNO3OvQ=w1280'
+            body = '30 min sessions for $50 / 45 min sessions for $75 / 60 min sessions for $90' / >
             <
             /div>
-
         )
     }
 }
-
 export default pricing
 
-function PriceCard(props) {
-    return ( <
-        div className = "card" >
-        <
-        div className = "card__element" >
-        <
-        img src = { props.img }
-        /> <
-        h2 className = "service__name" > { props.name }
 
+function PriceCard({ service, image, body }) {
+    return ( <
+        div className = 'service-position' >
         <
-        /h2> <
-        p className = "service__details" > { props.description } < /p> <
-        button className = "card__button" > View Service < /button> < /
+        div className = "image-position" >
+        <
+        img src = { image }
+        alt = '' / >
+        <
+        /div>     <
+        div className = "service-content" >
+        <
+        div className = "service-name" >
+        <
+        h3 > { service } < /h3>       < /
+        div > <
+        div className = "service-text" >
+        <
+        p > { body } < /p>   < /
+        div > <
+        div className = "btn" >
+        <
+        button >
+        <
+        a > View more <
+        /a>    < /
+        button > <
+        /div>   < /
         div > <
         /div>
     )
