db47fed29b22dc8ac5518c54ff08554ab3ebc6a6
Kelvin Wong <kakoo40@gmail.com>
2022-01-17 13:14:16 +1300

    Added logo and changed text location

--- takapuna-massage-frontend/src/components/Navbar.js
+++ takapuna-massage-frontend/src/components/Navbar.js
@@ -4,6 +4,9 @@ import Link from 'react-router-dom/Link';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+import logo from '../images/logo.png';
 
 
 export class Navbar extends Component {
@@ -11,6 +14,11 @@ export class Navbar extends Component {
         return (
             <AppBar>
                 <Toolbar className="nav-container">
+                    <IconButton edge="start" color="inherit"> 
+                        <Button color="inherit" component={Link} to="/">
+                        <img classname="icon" src={logo} alt="Logo" height="50px" width="50px" />
+                        </Button>
+                    </IconButton>
                     <Button color="inherit" component={Link} to="/">Home</Button>
                     <Button color="inherit" component={Link} to="/booking">Book now</Button>
                     <Button color="inherit" component={Link} to="/about">About us</Button>
@@ -22,4 +30,6 @@ export class Navbar extends Component {
     }
 }
 
-export default Navbar
+
+
+export default Navbar;
