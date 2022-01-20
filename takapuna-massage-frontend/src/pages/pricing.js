7979867f9c8486b0bda3dadba102f6c124304206
nst2000 <sennoel2000@outlook.co.nz>
2022-01-20 18:43:54 +1300

    Fixed Cards into Grid Format
    Cards can be resized, added color to the background, text and button has been centered

--- takapuna-massage-frontend/src/pages/pricing.js
+++ takapuna-massage-frontend/src/pages/pricing.js
@@ -6,203 +6,219 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row
 
 export class pricing extends Component {
     render() {
-        return ( <
-            div className = "Card" >
-            <
-            Container >
-            <
-            Row >
-            <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
-            src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" > Upper & Lower Body < /CardTitle>   <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
-            'Neck, Shoulders and Back: Back and Leg 30 min sessions for $50 / 45 min sessions for $70 / 60 min sessions for $85 </CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button >
-            View More < /Button>   < /
-            CardBody >
+        return ( 
+            <div className = "card text-center" >
+            <Container>
+            <Row>
+            <Col md = { 4 }>
+            <Card body style = 
+            {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+            <CardImg top width = "100%"
+            src = "https://picsum.photos/318/180" />
 
-            <
-            /Card> < /
-            Col >
-            <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
-            src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" >
-            Whole Body < /CardTitle>  <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
+            <CardBody body color = "light" >
+            <CardTitle tag = "h5" > Upper & Lower Body </CardTitle>      
+            <CardSubtitle>
+            </CardSubtitle>    
+                                    <CardText className="mb-2 text-muted" tag="h6" color='white'>
+                                        30 min sessions
+            for $50 / 45 min sessions
+            for $70 / 60 min sessions
+            for $85 
+            </CardText>     
+            <Button align = 'center' color = "secondary" > View More </Button>     
+            </CardBody> 
+            </Card>  
+             </Col>
+            <Col md = { 4 }>
+        <Card body style= {
+                {
+                    textAlign: 'center',
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+            <CardImg top width = "100%"
+            src = "https://picsum.photos/318/180"/>
+            <CardBody>
+            <CardTitle tag = "h5" >
+          Whole Body </CardTitle>
+            <CardSubtitle>
+        </CardSubtitle>
+    <CardText className="mb-2 text-muted" tag="h6" >
             45 min sessions
             for $70 / 60 min sessions
-            for $85, 90 min sessions
+            for $85 / 90 min sessions
             for $125 / 120 min sessions
-            for $160 <
-            /CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button > View More < /Button>  < /
-            CardBody >
-
-            <
-            /Card> < /
-            Col > <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
+            for $160
+        </CardText>
+        <Button> View More </Button>
+             </CardBody>
+             </Card>
+             </Col>
+          <Col md = { 4 } >
+            <Card body style = {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+            <CardImg top width = "100%"
             src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" >
-            Reflexology Foot Massage < /CardTitle>  <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
+            <CardBody>
+            <CardTitle tag = "h5"> Reflexology Foot Massage </CardTitle>
+           <CardSubtitle className = "mb-2 text-muted" tag = "h6" >
             30 min sessions
             for $50 / 45 min sessions
             for $75 / 60 min sessions
-            for $90 <
-            /CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button > View More < /Button>  < /
-            CardBody >
-
-            <
-            /Card> < /
-            Col > <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
-            src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" >
-            Head Neck and Shoulders < /CardTitle>  <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
+           for $90
+         </CardSubtitle>
+         <CardText>
+        </CardText>
+     <Button> View More </Button>
+    </CardBody>
+ </Card>
+ </Col>
+<Col md={4}>
+    <Card body inverse style = {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+    <CardImg top width = "100%"
+     src = "https://picsum.photos/318/180" / >
+    <CardBody>
+    <CardTitle tag="h5"> Head Neck and Shoulders </CardTitle>
+    <CardSubtitle className = "mb-2 text-muted" tag = "h6" >
             10 min sessions
             for $20 / 20 min sessions
             for $35 / 30 min sessions
-            for $50 <
-            /CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button > View More < /Button>  < /
-            CardBody >
-
-            <
-            /Card> < /
-            Col >
-            <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
+         for $50
+    </CardSubtitle>
+<CardText>
+</CardText>
+ <Button> View More </Button>
+ </CardBody>
+</Card>
+ </Col>
+ <Col md = { 4 } >
+<Card body inverse style = {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+<CardImg top width = "100%"
             src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" >
-            Head Neck and Shoulders < /CardTitle>  <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
+<CardBody>
+    <CardTitle tag = "h5" >
+    Head Neck and Shoulders </CardTitle>
+ <CardSubtitle className = "mb-2 text-muted" tag = "h6" >
             10 min sessions
             for $20 / 20 min sessions
             for $35 / 30 min sessions
-            for $50 <
-            /CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button > View More < /Button>  < /
-            CardBody >
-
-            <
-            /Card> < /
-            Col > <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
+         for $50 </CardSubtitle>
+     <CardText>
+    </CardText>
+<Button> View More </Button>
+ </CardBody>
+</Card>
+</Col>
+<Col md = { 4 }>
+<Card body inverse style = {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+<CardImg top width = "100%"
             src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" >
-            Head Neck and Shoulders < /CardTitle>  <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
+    <CardBody>
+<CardTitle tag = "h5">
+ Head Neck and Shoulders </CardTitle>
+ <CardSubtitle className = "mb-2 text-muted" tag = "h6" >
             10 min sessions
             for $20 / 20 min sessions
             for $35 / 30 min sessions
-            for $50 <
-            /CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button > View More < /Button>  < /
-            CardBody >
-
-            <
-            /Card> < /
-            Col > <
-            Col md = { 3 } >
-            <
-            Card >
-            <
-            CardImg top width = "100%"
+        for $50
+     </CardSubtitle>
+ <CardText>
+    </CardText>
+     <Button> View More </Button>
+     </CardBody>
+    </Card>
+                        </Col>
+                        <Col md = { 4 } >
+            <Card body inverse style = {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+            <CardImg top width = "100%"
             src = "https://picsum.photos/318/180" / >
-            <
-            CardBody >
-            <
-            CardTitle tag = "h5" >
-            Head Neck and Shoulders < /CardTitle>  <
-            CardSubtitle className = "mb-2 text-muted"
-            tag = "h6" >
+            <CardBody>
+            <CardTitle tag = "h5"> Head Neck and Shoulders </CardTitle>
+        <CardSubtitle className = "mb-2 text-muted" tag = "h6" >
             10 min sessions
             for $20 / 20 min sessions
             for $35 / 30 min sessions
-            for $50 <
-            /CardSubtitle> <
-            CardText >
-            Some quick example text to build on the card title and make up the bulk of the card 's content. </CardText>  <
-            Button > View More < /Button>  < /
-            CardBody >
-
-            <
-            /Card> < /
-            Col > <
-            /Row>
-
-            <
-            /Container>
-
-
-
-            <
-            /div>
+          for $50
+        </CardSubtitle>
+    <CardText>
+      </CardText>
+    <Button> View More </Button>
+</CardBody>
+</Card>
+                        </Col>
+                        <Col md = { 4 } >
+            <Card body inverse style = {
+                {
+                    backgroundColor: '#f25c2d',
+                    borderColor: '#d00000',
+                    borderWidth: 5,
+                    borderStyle: 'groove'
+                }
+            } >
+            <CardImg top width = "100%"
+            src = "https://picsum.photos/318/180" / >
+            <CardBody>
+            <CardTitle tag = "h5"> Head Neck and Shoulders </CardTitle>
+        <CardSubtitle className = "mb-2 text-muted" tag = "h6" >
+            10 min sessions
+            for $20 / 20 min sessions
+            for $35 / 30 min sessions
+          for $50
+        </CardSubtitle>
+    <CardText>
+      </CardText>
+    <Button> View More </Button>
+</CardBody>
+</Card>
+  </Col>
+  </Row>
+</Container>
+</div>
         )
     }
 }
