24d856cd89a74bc540fd6dd96cee09a896381ee6
Kelvin Wong <kakoo40@gmail.com>
2022-01-24 20:27:44 +1300

    Update home.js

--- takapuna-massage-frontend/src/pages/home.js
+++ takapuna-massage-frontend/src/pages/home.js
@@ -87,18 +87,3 @@ export class home extends Component {
 }
 
 export default home;
-
-/*<div className='head-text'>
-                    <div className='head-image'>
-                        <img src={artic} alt="Stones" width="1252px" height="834px" />
-                    </div>
-                    <div className='text-on-image'>
-                        <h1>Takapuna Therapeutic Massage is a Professional Traditional Massage shop in Takapuna North Shore. 
-                            We also sell massage products such as patches, medical ointment and massagers.
-                            Our therapeutic massages include traditional Taiwanese and Chinese style acupressure medicinal techniques. 
-                            Our masseurs have at least 5 to 20 years of massage experience. 
-                            The environment in our store is comfortable and relaxing and our massage therapists are professional and friendly. 
-                            We provide soft music during treatments with complimentary refreshing health tea after each session. 
-                            Treat yourself to a massage -let yourself go, free your mind and calm your body and soul. </h1>
-                    </div>
-                </div>*/
