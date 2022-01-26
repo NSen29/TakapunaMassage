8ccff05c8470d1471840d1e0d362a02c0ce5e2bd
Kelvin Wong <kakoo40@gmail.com>
2022-01-26 17:40:10 +1300

    Added employees

--- takapuna-massage-frontend/src/pages/about.js
+++ takapuna-massage-frontend/src/pages/about.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Row,
+  Col,
+  Container,
+} from "reactstrap";
+
 import carousel1 from "../images/carousel1.jpeg";
 import carousel2 from "../images/carousel2.jpeg";
 import carousel3 from "../images/carousel3.jpeg";
@@ -10,6 +22,8 @@ import carousel5 from "../images/carousel5.jpeg";
 import carousel6 from "../images/carousel6.jpeg";
 import carousel7 from "../images/carousel7.jpeg";
 import carousel8 from "../images/carousel8.jpeg";
+
+import "../styles/About.css";
 
 export class about extends Component {
   render() {
@@ -57,14 +71,149 @@ export class about extends Component {
             </div>
           </Carousel>
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>Employees</h1>
+
+        <div className="card text-center ">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1>Employees</h1>
+          </div>
+          <Container className="gap">
+            <Row>
+              <Col md={6}>
+                <Card
+                  body
+                  style={{
+                    backgroundColor: "#e07006",
+                    borderColor: "#f33e3e",
+                    borderWidth: 0,
+                    borderStyle: "groove",
+                  }}
+                  inverse
+                >
+                  <CardImg
+                    top
+                    width="100%"
+                    src={require("../images/Roy.png")}
+                    alt="Roy"
+                  />
+
+                  <CardBody body color="light">
+                    <CardTitle tag="h5">Roy Hsu</CardTitle>
+                    <CardSubtitle className="mb-2 text-white" tag="h6">
+                      More than 20 years of experience in professional
+                      therapeutic massage. <br />
+                      Traditional Chinese Therapy and Arcmatherapy, Original
+                      Point Therapy, Relaxation Massage. <br />
+                      Licensed by Taiwan Rehabilitation Practitioners
+                      Association. <br />
+                      Hot Stone, Gua Sha, Cupping, Moxa Therapy, Transcutaneous
+                      Electrical Nerve Stimulation, Health Management.
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col md={6}>
+                <Card
+                  body
+                  style={{
+                    backgroundColor: "#8b0000",
+                    borderColor: "#d00000",
+                    borderWidth: 0,
+                    borderStyle: "groove",
+                  }}
+                  inverse
+                >
+                  <CardImg
+                    top
+                    width="100%"
+                    src={require("../images/Na Lan.png")}
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">Na Lan</CardTitle>
+                    <CardSubtitle className="mb-2 text-white" tag="h6">
+                      International Licensed Acupuncture Therapist. <br />
+                      Six-year licensed acupuncturist and physiotherapist in
+                      China Medical Clinic. <br />
+                      Licensed Acupuncture Treatment in Australia. <br />
+                      Chronic disease research, diagnosis, and treatment.
+                      Gynecological treatement <br />
+                      Pediatric Physiotherapy. <br />
+                      Pulse, Acupuncture, Scraping, Moxibustion, Health
+                      Management.
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col md={6}>
+                <Card
+                  body
+                  style={{
+                    textAlign: "center",
+                    backgroundColor: "#212121",
+                    borderColor: "#d00000",
+                    borderWidth: 0,
+                    borderStyle: "groove",
+                  }}
+                  inverse
+                >
+                  <CardImg
+                    top
+                    width="100%"
+                    src={require("../images/Vivian.png")}
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">Vivian</CardTitle>
+                    <CardSubtitle className=" text-white">
+                      More than 5 years of experience in professional
+                      therapeutic massage. <br />
+                      Traditional Chinsese Therapy and Association, Relaxation
+                      Massage, Sport Massage. <br />
+                      Hot Stone, Gua Sha, Cupping, Moxa Therapy, Transcutaneous
+                      Electrical Nerve Stimulation, Health Management
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col md={6}>
+                <Card
+                  body
+                  style={{
+                    backgroundColor: "#e07006",
+                    borderColor: "#d00000",
+                    borderWidth: 0,
+                    borderStyle: "groove",
+                  }}
+                  inverse
+                >
+                  <CardImg
+                    top
+                    width="100%"
+                    src={require("../images/Sunny.png")}
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5"> Sunny </CardTitle>
+                    <CardSubtitle className="mb-2 text-white" tag="h6">
+                      More than 6 years of experience in professional
+                      therapeutic massage. <br />
+                      Traditional Chinsese Therapy and Association, Relaxation
+                      Massage, Sport Massage. <br />
+                      Hot Stone, Gua Sha, Cupping, Moxa Therapy, Transcutaneous
+                      Electrical Nerve Stimulation, Health Management
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+          <br />
         </div>
       </div>
     );
